b56bd97e0b7dbcf61fbd5580d019543a61d33246
sushant2931 <sushantlad8888@gmail.com>
2021-09-17 00:54:52 +0530

    REFACTORED:Method To ensure Mail Contain At Least one @

--- src/com/bl/emailvalidator/EmailValidator.java
+++ src/com/bl/emailvalidator/EmailValidator.java
@@ -4,31 +4,35 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailValidator {
-    public static void checkValidEmail(String email) {
+    public static void checkValidEmail(String email){
 
         boolean isEmail;
-        String emailRegex = "^[a-zA-Z][.@a-zA-Z]*$";
+        String emailRegex = "^[a-zA-Z]*[@]{1}[.a-z]*$";
         Pattern patternObject = Pattern.compile(emailRegex);
         if (email == null) {
             isEmail = false;
         }
         Matcher matcherObject = patternObject.matcher(email);
-        isEmail = matcherObject.matches();
+        isEmail =  matcherObject.matches();
 
-        if (isEmail)
-            System.out.println(email + " is a Valid Email address");
+        if(isEmail)
+            System.out.println(email+" is a Valid Email address");
         else
-            System.out.println(email + " is an Invalid Email address");
+            System.out.println(email+" is an Invalid Email address");
     }
 
     public static void main(String[] args) {
 
         System.out.println("---------- Welcome To Email Validator ----------");
 
-        String email = "abc.xyz@bridgelabz.co.in";
+        String email = "abc@bridgelabz.co.in";
         checkValidEmail(email);
 
         email = ".abc@abc.com";
         checkValidEmail(email);
+
+        email = "abc@gmail.com.com";
+        checkValidEmail(email);
     }
+
 }
