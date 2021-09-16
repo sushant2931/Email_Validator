c567e7236e78e24395ab6156d5d1765229cf6176
sushant2931 <sushantlad8888@gmail.com>
2021-09-17 00:50:25 +0530

    REFRACTORED: method tu ensure Email Begins With Alphabete

--- src/com/bl/emailvalidator/EmailValidator.java
+++ src/com/bl/emailvalidator/EmailValidator.java
@@ -1,7 +1,34 @@
 package com.bl.emailvalidator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidator {
+    public static void checkValidEmail(String email) {
+
+        boolean isEmail;
+        String emailRegex = "^[a-zA-Z][.@a-zA-Z]*$";
+        Pattern patternObject = Pattern.compile(emailRegex);
+        if (email == null) {
+            isEmail = false;
+        }
+        Matcher matcherObject = patternObject.matcher(email);
+        isEmail = matcherObject.matches();
+
+        if (isEmail)
+            System.out.println(email + " is a Valid Email address");
+        else
+            System.out.println(email + " is an Invalid Email address");
+    }
+
     public static void main(String[] args) {
+
         System.out.println("---------- Welcome To Email Validator ----------");
+
+        String email = "abc.xyz@bridgelabz.co.in";
+        checkValidEmail(email);
+
+        email = ".abc@abc.com";
+        checkValidEmail(email);
     }
 }
