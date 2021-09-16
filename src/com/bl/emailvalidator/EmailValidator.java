c6206805750a01e81e370074c70eb4316c9d4be8
sushant2931 <sushantlad8888@gmail.com>
2021-09-17 00:42:22 +0530

    Added: Welcome To  Email Validator

---
+++ src/com/bl/emailvalidator/EmailValidator.java
@@ -0,0 +1,7 @@
+package com.bl.emailvalidator;
+
+public class EmailValidator {
+    public static void main(String[] args) {
+        System.out.println("---------- Welcome To Email Validator ----------");
+    }
+}
